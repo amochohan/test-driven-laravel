a9973e9299d2b7f53aa1b465849de8ba80a0d019
Amo Chohan <amo.chohan@gmail.com>
2016-08-04 16:04:45 +0100

    Add CartController
    Notice here I have written the code I wish that I could use. It reads so simply.
    
    Load the product, and then for the auth'ed user's cart, add the product.

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class CartController extends Controller
+{
+    public function store(Request $request)
+    {
+        $product = Product::findOrFail($request->input('product'));
+
+        \Auth::user()->cart()->add($product);
+
+        return redirect()->route('cart.show');
+    }
+}
