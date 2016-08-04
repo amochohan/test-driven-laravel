5db05314be3386d0508ca4a1402ad2b6da904ede
Amo Chohan <amo.chohan@gmail.com>
2016-08-04 15:03:59 +0100

    Add the Product model factory
    Model factories allow us to use mocks of our models in tests.
    
    It enables us to spawn lots of instances of a product which can be immediately wiped after each test.
    
    This is also a great opportunity to consider which attributes we feel our model needs.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,20 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+
+    $sizes = ['large', 'small', 'medium'];
+    $colours = ['blue', 'red', 'green'];
+
+    // The format of our product name will be size + colour + word
+    // Large blue something
+
+    return [
+        'name' => ucfirst(sprintf('%s %s %s', $faker->randomElement($sizes), $faker->randomElement($colours), $faker->word)),
+        'description' => $faker->sentence,
+        'price' => $faker->numberBetween(500, 5000),
+        'image' => $faker->imageUrl(300, 200, 'food'),
+    ];
+
+});
