15f46c7bf24d2d4247e5244e6d6ac2804564c043
Amo Chohan <amo.chohan@gmail.com>
2016-08-04 15:07:00 +0100

    Add the catalogue controller
    Our route exists, and can respond to a request, but there the controller doesn't exist so the code halts and an exception is thrown.
    
    Here we create an empty controller to continue with the test.

---
+++ app/Http/Controllers/ProductCatalogueController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+class ProductCatalogueController extends Controller
+{
+    
+}
