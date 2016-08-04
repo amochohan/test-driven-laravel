4fc63dbac69ada4100e70f12f7170ba2f4853c5b
Amo Chohan <amo.chohan@gmail.com>
2016-08-04 16:06:02 +0100

    Add User integration test
    No we're demonstrating the outside-in approach. We begun with a failing acceptance test, which is still red.
    
    To move further along in the test, we need to start testing at a lower level, as our Controller is now defining logic on the User class, which doesn't exist.
    
    To build that feature, we need a test.

---
+++ tests/integration/UserTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+}
