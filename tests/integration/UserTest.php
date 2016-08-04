2b9dbe508bbf3a425f29fe9399335bb2b00d5958
Amo Chohan <amo.chohan@gmail.com>
2016-08-04 16:09:01 +0100

    Add a failing test for users having a cart
    Notice that we haven't followed the given, when, then notation in this test.
    
    It's important to remember that whilst useful, it's not possible to apply that in all of our tests. It works really well for acceptance, or functional tests.

--- tests/integration/UserTest.php
+++ tests/integration/UserTest.php
@@ -7,5 +7,12 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class UserTest extends TestCase
 {
     use DatabaseMigrations;
-    
+
+    /** @test */
+    public function a_user_has_a_cart()
+    {
+        $user = factory(App\User::class)->create();
+        $user->cart()->save(factory(App\Cart::class)->create());
+        $this->assertInstanceOf(App\Cart::class, $user->cart);
+    }
 }
