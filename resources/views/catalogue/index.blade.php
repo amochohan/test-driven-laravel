037424ae5b035b7b2b377b26ffd881f94afebc3e
Amo Chohan <amo.chohan@gmail.com>
2016-08-04 15:09:59 +0100

    Bring the test to green
    The feature is now complete and our tests are green. Congratulations, you've just written your first test-driven feature in Laravel.

--- resources/views/catalogue/index.blade.php
+++ resources/views/catalogue/index.blade.php
@@ -7,6 +7,37 @@
     <p>
         Total products: {{ $products->count() }}
     </p>
-    
+
+    @foreach($products->chunk(3) as $products)
+
+        <div class="row">
+            @foreach ($products as $product)
+
+                <div class="col-md-4">
+                    <!-- Product application panel -->
+                    <div class="panel panel-default">
+
+                        <div class="panel-heading">
+                            {{ $product->name }}
+                        </div>
+
+                        <div class="panel-body">
+
+                            <img src="{{ $product->image }}" />
+
+                            <p>{{ $product->description }}</p>
+
+                            <p><strong>{{ $product->price }}</strong></p>
+
+                        </div>
+
+                    </div>
+                </div>
+
+            @endforeach
+        </div>
+
+    @endforeach
+
 
 @endsection
