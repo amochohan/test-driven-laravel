6ce81e985365dbf250a05999931f156132eef9d8
Amo Chohan <amo.chohan@gmail.com>
2016-08-04 16:03:25 +0100

    Add a form to add the product to the cart

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -10,4 +10,18 @@
 
     <p>Price: &pound;{{ $product->price }}</p>
 
+    <!-- Add to cart form -->
+    <form method="post" id="add-to-cart" name="add_to_cart" action="{{ route('cart.store') }}">
+
+        {{ csrf_field() }}
+
+        <input type="hidden" name="product" value="{{ $product->id }}" />
+
+        <!-- Submit button -->
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Add to cart</button>
+        </div>
+
+    </form>
+
 @endsection
